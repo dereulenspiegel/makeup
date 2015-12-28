0e15a7b1673854bc49a7578615f3623b9997016e
Till Klocke <till.klocke@gmail.com>
2015-12-28 11:39:24 +0100

    The data type is now determined automatically which makes the usage when piping much easier.

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 
@@ -13,34 +13,53 @@ import (
 var inFile = flag.String("in", "", "Input file")
 var fileType = flag.String("type", "", "Type of input (json)")
 
+func determineDataType(in []byte) string {
+	for _, char := range in {
+		if char == '{' || char == '[' {
+			return "json"
+		}
+		if char == '<' {
+			return "xml"
+		}
+	}
+	return ""
+}
+
 func main() {
 	flag.Parse()
+	var uglyData []byte
+	var prettyData []byte
+	var err error
+	if *inFile != "" {
+		uglyData, err = ioutil.ReadFile(*inFile)
+		if err != nil {
+			log.Fatalf("Can't read file %s: %v", *inFile, err)
+		}
+	} else {
+		uglyData, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalf("Can't read from stdin: %v", err)
+		}
+	}
 	var inType string
-	if *fileType == "" {
+	if *fileType == "" && *inFile != "" {
 		lastDot := strings.LastIndex(*inFile, ".")
 		fileName := *inFile
 		inType = fileName[lastDot+1:]
-	} else {
+	} else if *fileType != "" {
 		inType = *fileType
+	} else {
+		inType = determineDataType(uglyData[:32])
 	}
 	cosmetics := makeup.GetCosmetics(inType)
 
 	if cosmetics != nil {
-		var err error
-		var out []byte
-		if *inFile == "" {
-			var in []byte
-			in, err = ioutil.ReadAll(os.Stdin)
-			out, err = cosmetics.Prettify(in)
-		} else {
-			out, err = cosmetics.PrettifyFile(*inFile)
-		}
+		prettyData, err = cosmetics.Prettify(uglyData)
 		if err != nil {
-			fmt.Printf("Error prettyfying file %s: %v", *inFile, err)
-		} else {
-			fmt.Printf("%s", string(out))
+			log.Fatalf("Can't prettify input data: %v", err)
 		}
+		os.Stdout.Write(prettyData)
 	} else {
-		fmt.Printf("Unknown format %s", inType)
+		log.Fatalf("Can't determine type of input data")
 	}
 }
